7a47e9a105dc64eb997c53dbaaff4352195499b3
Alaksandu <alexandre.nike10@hotmail.com>
2022-03-25 15:13:23 -0300

    primeiro commit

---
+++ Scripts/CriandoDatabaseSQL.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE db_Biblioteca
+ON PRIMARY (
+NAME=db_Biblioteca,
+FILENAME='C:\Users\Alaksandu\workspace\revisao-tsql\Bases_MDF\db_Biblioteca',
+SIZE=6MB,
+MAXSIZE=15MB,
+FILEGROWTH=10%
+)
